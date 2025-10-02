a41acb7556f91a1c6bec106d520298eb02ac1314
Seth Forte <sethforte1@email.com>
2025-10-01 20:30:42 -0500

    pushing changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -325,6 +325,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            
 
             {/* Services Cards */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
