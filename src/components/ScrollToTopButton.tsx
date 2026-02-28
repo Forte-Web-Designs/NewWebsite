dfa6627b6670df41d6a588f255078502c4b11db4
Seth Forte <sethforte1@gmail.com>
2026-02-27 20:10:17 -0600

    push

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -22,7 +22,7 @@ export default function ScrollToTopButton() {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-20 right-4 md:bottom-6 md:right-6 z-40 flex flex-col gap-2">
+    <div className="fixed bottom-6 right-4 md:right-6 z-40 flex flex-col gap-2">
       {/* Theme toggle */}
       {mounted && (
         <button
